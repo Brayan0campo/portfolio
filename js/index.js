4320ca46cb881179f093f2b7373b4514b7b7e4b7
Brayan0campo <ocamporodriguezbrayan@gmail.com>
2023-07-24 18:17:44 -0500

    change load-page

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
-document.onreadystatechange = function() {
+document.addEventListener("DOMContentLoaded", function() {
+    const gifImage = document.getElementById("load-image");
+    gifImage.src = gifImage.src;
+});
+
+document.onreadystatechange = function () {
     if (document.readyState !== "complete") {
         document.querySelector("body").style.visibility = "hidden";
         document.querySelector(".load-page").style.visibility = "visible";
@@ -8,7 +13,7 @@ document.onreadystatechange = function() {
             document.querySelector(".load-page").style.visibility = "hidden";
             document.querySelector(".load-page").style.opacity = "0";
             document.querySelector(".load-page").style.transform = "translateY(100%)";
-        }, 3700);
+        }, 3600);
         setTimeout(() => {
             document.querySelector(".load-page").style.display = "none";
         }, 7000);
